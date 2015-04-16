98b7fdce70a7f3292dd1d14fddc031a0119b8781
黄毅 <yi.codeplayer@gmail.com>
2015-04-16 16:58:02 +0800

    fix wdj

--- sdks/wdj/src/org/yunyue/GameProxyImpl.java
+++ sdks/wdj/src/org/yunyue/GameProxyImpl.java
@@ -144,4 +144,9 @@ public class GameProxyImpl extends GameProxy {
     public boolean supportCommunity() {
         return false;
     }
+
+    public boolean supportLogout() {
+        return true;
+    }
+
 }
