963a89d282891a5d6649cbfd6c53d732a44cd652
黄毅 <yi.codeplayer@gmail.com>
2015-05-13 18:06:50 +0800

    refactor

--- processor.py
+++ processor.py
@@ -15,3 +15,28 @@ def process(rules):
     for pattern, command, args in rules:
         for path in glob.glob(pattern):
             globals()['command_'+command](path, args)
+
+
+class Rule(object):
+    @classmethod
+    def common_replaces(cls):
+        r = {}
+        for k in dir(cls):
+            v = getattr(cls, k)
+            if isinstance(v, basestring) and not k.startswith('__'):
+                r[k] = v
+        return r
+
+    @classmethod
+    def rules(cls):
+        return [
+            ('AndroidManifest.xml', 'replace', cls.common_replaces()),
+            ('src/org/yunyue/GameProxyImpl.java', 'replace', cls.common_replaces()),
+        ]
+
+all_rules = {}
+
+
+def register(cls):
+    all_rules[cls.LABEL] = cls
+    return cls
--- rules.py
+++ rules.py
@@ -1,48 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 import os
-from processor import process
+from processor import process, register, Rule, all_rules
 
 
-def register(container):
-    def decor(cls):
-        container[cls.LABEL] = cls
-        return cls
-    return decor
+class RuleBase(Rule):
+    VERSION_CODE = '120005'
+    VERSION_NAME = '1.20005'
 
 
-common_rules = [
-    ('AndroidManifest.xml', 'replace', {
-        'VERSION_CODE': '120005',
-        'VERSION_NAME': '1.20005',
-    }),
-]
-
-
-g_rules = {}
-
-
-class Rule(object):
-    @classmethod
-    def common_replaces(cls):
-        r = {}
-        for k in dir(cls):
-            v = getattr(cls, k)
-            if isinstance(v, basestring) and not k.startswith('__'):
-                r[k] = v
-        return r
-
-    @classmethod
-    def rules(cls):
-
-        return common_rules + [
-            ('AndroidManifest.xml', 'replace', cls.common_replaces()),
-            ('src/org/yunyue/GameProxyImpl.java', 'replace', cls.common_replaces()),
-        ]
-
-
-@register(g_rules)
-class RuleZC(Rule):
+@register
+class RuleZC(RuleBase):
     LABEL = 'zc'
     DIRECTORY = 'zc'
     CH_NAME = '筑巢小包'
@@ -55,7 +23,7 @@ class RuleZC(Rule):
     ZC_RSA = 'MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAOUust2OfurxzdifqELDYBBM44tUmqa/Sfqn90MkrjpORe78BuiUU25+JWzxizrgLgSicjH/RHZvWZs2CcZ7v2Q+cjlbMeqth/rDk5p2HW0jpEohkguwKt+2RenPTHzvRhLQ0bdYpYVQN6g8qPOVkkd1rsSVaz69PF5ENljJyw3JAgMBAAECgYEAvTRtdF4Ex8Ay+ejtR5j2gN6JaGjDeHAqCiaLCsKImBgwwhkNNwvlSS4ZhbRwBn43X5og/sfIZKKO7oWRUmytVuubR9eXsKTZGnrN8HgxEvGhlDoEBFu1GoEMmrD+XMAMsC2kKBgxjy315Ildz9LXB8tBMh1FjlQWJIXWUOMiSXUCQQD9uY34hHDFn5Fu1cLTRn8xk0vYjQzYp7FUty5k5CUMGhuSG4jXUJBHDoeXla51vyF74Qq1DOSQW6hVDMeSSa7XAkEA5zzOGfOLYbQhkD5SP6UdfBjeBJ6Em8i0lyuM/xTxEHgs9x8x2UdVUy0BSaVkrjxC4SInaM3ZIQN5o+rdvhy0XwJBANkT03KnpXB/eEdSnjBy5Un+EutAqpgGyUKIwynQxB2ZjLMx2Z8WL4qL1NiNWMkm8LfzL1z9neQgd2Hk4C652dsCQCAkramh1yAvv/KjFx/NvfmAI2yU9G4LSj8xSJo0uQXHDskTRwSjC9NSEDnCiepGai2NZ9kDtEkIiKImhchliRUCQHRacVqhfPqVjmx9HugQt1FvDnq1T+nKsYImZSKYi8oGGItXOX+alRIcflOWZk3zUpNO2Sbdx9LaugznUlYzib8='
 
 
-@register(g_rules)
+@register
 class RuleZCBaidu(RuleZC):
     LABEL = 'zc_baidu'
     CH_NAME = '筑巢百度小包'
@@ -63,14 +31,14 @@ class RuleZCBaidu(RuleZC):
     TD_APPID = '2c09c858fb8d4b468fdb26427ec6d18c'
 
 
-@register(g_rules)
+@register
 class RuleZCShenma(RuleZC):
     LABEL = 'zc_shenma'
     CH_NAME = '筑巢神马小包'
     TD_APPID = '8f7101f78ab24b34b75c07709bf9cff0'
 
 
-@register(g_rules)
+@register
 class RuleZCSougou(RuleZC):
     LABEL = 'zc_sougou'
     CH_NAME = '筑巢搜狗小包'
@@ -90,5 +58,6 @@ def main(desc):
         # TODO copy package
         os.chdir('../..')
 
+
 if __name__ == '__main__':
-    main(g_rules)
+    main(all_rules)
