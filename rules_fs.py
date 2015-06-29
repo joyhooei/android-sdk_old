64e09138e65b8178541fb6b2e822b397be684c75
黄毅 <yi.codeplayer@gmail.com>
2015-06-29 12:39:16 +0800

    百度参数

--- rules_fs.py
+++ rules_fs.py
@@ -34,3 +34,28 @@ class RuleUC(RuleBase):
         return super(RuleUC, cls).rules() + [
             ('src/com/ninegame/ucgamesdk/UCSdkConfig.java', 'replace', cls.common_replaces()),
         ]
+
+
+@register
+class RuleC360(RuleBase):
+    LABEL = 'c360'
+    DIRECTORY = 'c360'
+    CH_NAME = '360小包'
+    SDKTYPE = '19'
+    PACKAGE_NAME = 'com.yunyue.fs.c360'
+
+    QHOPENSDK_APPKEY = '7b6124fab627d2124667c807a757ece2'
+    QHOPENSDK_PRIVATEKEY = ''
+    QHOPENSDK_APPID = '202593846'
+
+
+@register
+class RuleBaidu(RuleBase):
+    LABEL = 'baidu'
+    DIRECTORY = 'baidu'
+    CH_NAME = '百度小包'
+    SDKTYPE = '17'
+    PACKAGE_NAME = 'com.yunyue.fs.baidu'
+
+    APPID = '6315322'
+    APPKEY = 'wLezE8VtTkO6rVkapuWCyxyP'
