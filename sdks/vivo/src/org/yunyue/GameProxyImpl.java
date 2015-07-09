adaacbba1d636ec3609ef2d58750e67c756bf450
黄毅 <yi.codeplayer@gmail.com>
2015-07-09 15:20:54 +0800

    fix vivo

--- sdks/vivo/src/org/yunyue/GameProxyImpl.java
+++ sdks/vivo/src/org/yunyue/GameProxyImpl.java
@@ -66,30 +66,33 @@ public class GameProxyImpl extends GameProxy {
     {
         public void handleMessage( android.os.Message msg )
         {
-            JSONObject jOrder = new JSONObject(mOrderInfo);
             if (msg.what == START_PAY)
             {
-                Bundle localBundle = new Bundle();
-                localBundle.putString("transNo", jOrder.getString("orderNumber"));// 交易流水号，由订单推送接口返回
-                localBundle.putString("accessKey", jOrder.getString("access_token"));// 由订单推送接口返回
-                localBundle.putString("productName", productInfo.productName);//商品名称
-                localBundle.putString("productDes", productInfo.productDesc);//商品描述
-                localBundle.putLong("price", (int)(Float.parseFloat(productInfo.price) * 100));//价格,单位为分（1000即10.00元）
-                localBundle.putString("appId", appid);// appid为vivo开发者平台中生成的App ID
+                try {
+                    JSONObject jOrder = new JSONObject(mOrderInfo);
+                    Bundle localBundle = new Bundle();
+                    localBundle.putString("transNo", jOrder.getString("orderNumber"));// 交易流水号，由订单推送接口返回
+                    localBundle.putString("accessKey", jOrder.getString("access_token"));// 由订单推送接口返回
+                    localBundle.putString("productName", productInfo.productName);//商品名称
+                    localBundle.putString("productDes", productInfo.productDesc);//商品描述
+                    localBundle.putLong("price", (int)(Float.parseFloat(productInfo.price) * 100));//价格,单位为分（1000即10.00元）
+                    localBundle.putString("appId", appid);// appid为vivo开发者平台中生成的App ID
 
-                // 以下为可选参数，能收集到务必填写，如未填写，掉单、用户密码找回等问题可能无法解决。
-                //localBundle.putString("blance", "100元宝");
-                //localBundle.putString("vip", "vip2");
-                //localBundle.putInt("level", 35);
-                //localBundle.putString("party", "工会");
-                //localBundle.putString("roleId", "角色id");
-                //localBundle.putString("roleName", "角色名称");
-                //localBundle.putString("serverName", "区服信息");
-                localBundle.putString("extInfo", productInfo.callBackInfo);
-                localBundle.putBoolean("logOnOff", false);// CP在接入过程请传true值,接入完成后在改为false, 传true会在支付SDK打印大量日志信息	 
-                Intent target = new Intent(currentActivity, PaymentActivity.class);
-                target.putExtra("payment_params", localBundle);
-                currentActivity.startActivityForResult(target, REQUEST_CODE_PAY);
+                    // 以下为可选参数，能收集到务必填写，如未填写，掉单、用户密码找回等问题可能无法解决。
+                    //localBundle.putString("blance", "100元宝");
+                    //localBundle.putString("vip", "vip2");
+                    //localBundle.putInt("level", 35);
+                    //localBundle.putString("party", "工会");
+                    //localBundle.putString("roleId", "角色id");
+                    //localBundle.putString("roleName", "角色名称");
+                    //localBundle.putString("serverName", "区服信息");
+                    localBundle.putString("extInfo", productInfo.callBackInfo);
+                    localBundle.putBoolean("logOnOff", false);// CP在接入过程请传true值,接入完成后在改为false, 传true会在支付SDK打印大量日志信息	 
+                    Intent target = new Intent(currentActivity, PaymentActivity.class);
+                    target.putExtra("payment_params", localBundle);
+                    currentActivity.startActivityForResult(target, REQUEST_CODE_PAY);
+                } catch (JSONException e) {
+                }
             }
         };
     };
