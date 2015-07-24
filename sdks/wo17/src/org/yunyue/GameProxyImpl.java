f2b52fd31629c5ad9be07253833feabc9f05fec5
黄毅 <yi.codeplayer@gmail.com>
2015-07-24 18:45:14 +0800

    17wo

--- sdks/wo17/src/org/yunyue/GameProxyImpl.java
+++ sdks/wo17/src/org/yunyue/GameProxyImpl.java
@@ -13,7 +13,17 @@ import android.os.Bundle;
 import android.os.Message;
 import android.widget.Toast;
 
+import com.future.playgame.coin.listenter.PlayGCInitSDKListener;
+import com.future.playgame.coin.listenter.PlayGCPayListener;
+import com.future.playgame.coin.listenter.PlayGCQueryInfoListener;
+import com.future.playgame.coin.manager.PlayGCConfigManager;
+import com.future.playgame.coin.manager.PlayGCManager;
+import com.future.playgame.coin.manager.PlayGCMessageUtils;
+
 public class GameProxyImpl extends GameProxy{
+
+    private static String appid = "${APPID}";
+    private static String appkey = "${APPNAME}";
 
     public boolean supportLogin() {
         return false;
@@ -24,7 +34,51 @@ public class GameProxyImpl extends GameProxy{
     }
 
     public boolean supportPay() {
-        return false;
+        return true;
     }
 
+    /**
+	 * 初始化SDK
+	 */
+	private void initSDK(Activity activity) {
+		PlayGCManager.instance().initSDK(activity,
+				new PlayGCInitSDKListener() {
+
+					@Override
+					public void onInitRstCode(int rstCode) {
+						if (rstCode == PlayGCMessageUtils.INIT_SDK_SUCCESS) {
+							resultText.setText("初始化成功");
+						} else if (rstCode == PlayGCMessageUtils.INIT_SDK_FAIL) {
+							resultText.setText("初始化失败");
+						} else if (rstCode == PlayGCMessageUtils.INIT_SDK_REPEAT) {
+							resultText.setText("已初始化成功");
+						}
+					}
+				});
+
+	}
+
+    public void applicationInit(Activity activity) {
+        initSDK(activity);
+    }
+
+    public void onDestroy(Activity activity) {
+        PlayGCManager.instance().closeSDK();
+    }
+
+    public void pay(Activity activity, String ID, String name, String orderID, float price, String callBackInfo, JSONObject roleInfo, final PayCallBack payCallBack) {
+        PlayGCManager.instance().intentPayActivity(
+                new PlayGCPayListener() {
+                    @Override
+                    public void onPayResult(int resultCode, Object payObj,int type) {
+                        Log.i("tag", "code:"+resultCode+" result:"+payObj.toString()+" type:"+type);
+                        if (resultCode == PlayGCMessageUtils.PAY_SUCCESS) {
+                            payCallBack.onSuccess("");
+                        } else {
+                            payCallBack.onFail("");
+                        }
+                    }
+                }, PlayGCManager.ACTIVITY_MMUNIPAY_FLAG, orderID, appid, "${APPNAME}",
+                    appkey, name, "1", String.valueOf((int)price), ID, "可用于购买道具");
+    }
 }
