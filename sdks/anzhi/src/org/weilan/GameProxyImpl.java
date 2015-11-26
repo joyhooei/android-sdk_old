2e54511f3b697be306a556de943ad662a527f2c9
xiaoshi <shi.xiao@winnergame.com>
2015-11-26 18:23:07 +0800

    anzhi remove unused file and merge

--- sdks/anzhi/src/org/weilan/GameProxyImpl.java
+++ sdks/anzhi/src/org/weilan/GameProxyImpl.java
@@ -19,40 +19,48 @@ import com.anzhi.usercenter.sdk.inter.InitSDKCallback;
 import com.anzhi.usercenter.sdk.item.CPInfo;
 import com.anzhi.usercenter.sdk.item.UserGameInfo;
 import com.anzhi.usercenter.sdk.inter.KeybackCall;
+import com.anzhi.usercenter.sdk.inter.AzOutGameInter;
 
 import ${PACKAGE_NAME}.R;
 
-public class GameProxyImpl extends GameProxy implements KeybackCall, InitSDKCallback, AnzhiCallback{
+public class GameProxyImpl extends GameProxy implements KeybackCall, InitSDKCallback, AnzhiCallback, AzOutGameInter{
     private AnzhiUserCenter mAnzhiCenter;
     private Activity currentActivity;
 
     private Object loginCustomParams;
     private Object logoutCustomParams;
     private PayCallBack payCallBack;
+    private ExitCallback mExitCallback;
 
-    private String TAG = "sdk";
+    public boolean supportLogin() {
+        // 是否支持sdk登录
+        return true;
+    }
+
+    public boolean supportCommunity() {
+        // 是否支持打开社区
+        return false;
+    }
+
+    public boolean supportPay() {
+        // 是否支持支付
+        return true;
+    }
 
     @Override
     public void applicationInit(Activity activity) {
-        Log.v("sdk", "applicationInit");
         currentActivity = activity;
         initInfo(activity);
     }
 
     @Override
-    public void onCreate(Activity activity) {
-        Log.v("sdk", "onCreate");
-    }
-
-    @Override
     public void onDestroy(Activity activity) {
         super.onDestroy(activity);
-        mAnzhiCenter.gameOver(activity);// 销毁悬浮球，在退出需要调用
     }
 
     @Override
     public void openCommunity(Activity activity) {
-        mAnzhiCenter.viewUserInfo(activity);
+        //mAnzhiCenter.viewUserInfo(activity);
     }
 
     @Override
@@ -67,6 +75,15 @@ public class GameProxyImpl extends GameProxy implements KeybackCall, InitSDKCall
         mAnzhiCenter.logout(activity);
     }
 
+    public void exit(Activity activity, ExitCallback callback) {
+        // 点返回键退出，默认调用游戏内的退出界面，如果sdk有要求则重载
+        /**
+         * 1、调用本方法或触发退出逻辑 2、在账号登录的情况下，首次调用本方法或展示后台配置的推广位，如果没有则弹Toast 3、三秒内重复调用本方法会发出退出游戏的通知回调。
+         */
+        mExitCallback = callback;
+        mAnzhiCenter.azoutGame(true);
+    }
+
     public void pay(Activity activity, String ID, String name, String orderID, float price, String callBackInfo, JSONObject roleInfo, PayCallBack payCallBack) {
         this.payCallBack = payCallBack;
         String s = callBackInfo + "_" + orderID.replace('-', '_');
@@ -74,28 +91,61 @@ public class GameProxyImpl extends GameProxy implements KeybackCall, InitSDKCall
         mAnzhiCenter.pay(activity, 0, price, name, s);
     }
 
+    public void setExtData(Context context, String ext) {
+        // 上报角色数据给sdk，ext:json数据，格式如下
+        /* local info = {
+         *     state = send_type,                   -- type
+         *     id = roleId,                      -- roleId
+         *     name = roleName,                    -- roleName
+         *     level = roleLevel,                   -- roleLevel
+         *     serverID = zoneId,                      -- zoneId
+         *     serverName = zoneName,                    -- zoneName
+         *     gold = balance,                     -- balance
+         *     vip = vip,                         -- vip 
+         *     factionName = partyName                    -- partyName 
+         * }
+         */
+        try {
+            JSONObject roleInfo = new JSONObject(ext);
+
+            UserGameInfo gameInfo = new UserGameInfo();
+            gameInfo.setNickName(roleInfo.optString("roleName"));
+            gameInfo.setUid(roleInfo.optString("id"));
+            gameInfo.setAppName("${APPNAME}");
+            gameInfo.setGameArea(roleInfo.optString("serverID"));// 游戏的服务器区
+            gameInfo.setGameLevel(roleInfo.optString("level"));// 游戏角色等级
+            gameInfo.setUserRole(roleInfo.optString("name"));// 角色名称
+            gameInfo.setMemo("");// 备注
+            mAnzhiCenter.submitGameInfo(context, gameInfo);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
     /*
      * 初始化SDK信息
      */
     private void initInfo(Activity activity) {
         final CPInfo info = new CPInfo();
-        // info.setOpenOfficialLogin(false);// 是否开启游戏官方账号登录，默认为关闭
+        info.setOpenOfficialLogin(false);// 是否开启游戏官方账号登录，默认为关闭
         info.setAppKey("${APPKEY}");
         info.setSecret("${APPSECRET}");
         info.setChannel("AnZhi");
         info.setGameName(activity.getResources().getString(R.string.app_name));
         mAnzhiCenter = AnzhiUserCenter.getInstance();
-        mAnzhiCenter.azinitSDK(activity, info, this);       // 初始化方法
-        mAnzhiCenter.setOpendTestLog(true);                     // 调试log，开关
+        mAnzhiCenter.azinitSDK(activity, info, this, this);       // 初始化方法
+        //mAnzhiCenter.setOpendTestLog(true);                     // 调试log，开关
         mAnzhiCenter.setKeybackCall(this);                  //设置返回游戏的接口
         mAnzhiCenter.setCallback(this);                     //设置登录、登出、支付通知
         mAnzhiCenter.setActivityOrientation(1);                 // 0横屏,1竖屏,4根据物理感应来选择方向
+
+        mAnzhiCenter.showFloaticon();
     }
 
     // 登录、登出、支付回调接口
+    // AnzhiCallback
     @Override
     public void onCallback(CPInfo cpInfo, String result) {
-        Log.e(TAG, "result " + result);
         try {
             JSONObject json = new JSONObject(result);
             String key = json.optString("callback_key");
@@ -117,16 +167,33 @@ public class GameProxyImpl extends GameProxy implements KeybackCall, InitSDKCall
         }
     }
 
-    //
+    // KeybackCall
     @Override
     public void KeybackCall(String st) {
         //Log.e(TAG, "st===========" + st);// 根据st来判断返回页面，st可能为空
     }
 
-    // 初始化接口所需实现的方法，SDK初始化之后回调此方法，在此方法中可以调用登录方法，完成自动登录的流程；
+    // InitSDKCallback
     @Override
-    public void ininSdkCallcack() {
-        //mAnzhiCenter.login(this, true);// 登录方法，第二个参数暂时不起任何作用，传true 即可
+    public void initSdkCallcack() {
+        //mAnzhiCenter.login(GameActivity.this, true);
     }
 
+    //AzOutGameInter
+    @Override
+    public void azOutGameInter(int arg) {
+
+        switch (arg) {
+            case AzOutGameInter.KEY_OUT_GAME:// 退出游戏
+                mAnzhiCenter.removeFloaticon(currentActivity);
+                if( mExitCallback != null )
+                    mExitCallback.onExit();
+                break;
+            case AzOutGameInter.KEY_CANCEL: // 取消退出游戏
+                Log.e("xugh", "-----");
+                break;
+            default:
+                break;
+        }
+    }
 }
