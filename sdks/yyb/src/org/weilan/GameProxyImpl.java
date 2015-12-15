f6e780d672f41b6609632fdb3b4eefaaf72905a0
xiaoshi <shi.xiao@winnergame.com>
2015-12-15 22:01:01 +0800

    close test

--- sdks/yyb/src/org/weilan/GameProxyImpl.java
+++ sdks/yyb/src/org/weilan/GameProxyImpl.java
@@ -372,7 +372,7 @@ public class GameProxyImpl extends GameProxy {
         //String discountUrl = "http://imgcache.qq.com/bossweb/midas/unipay/test/act/actTip.html?_t=1&mpwidth=420&mpheight=250";
 
         //充值游戏币接口，充值默认值由支付SDK设置;
-        unipayAPI.setEnv("test");
+        //unipayAPI.setEnv("test");
         unipayAPI.setLogEnable(true);
         UnipayPlugTools unipayPlugTools = new UnipayPlugTools(activity.getBaseContext());
         unipayPlugTools.setUrlForTest();
