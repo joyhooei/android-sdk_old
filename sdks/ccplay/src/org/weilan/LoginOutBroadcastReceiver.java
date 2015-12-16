ac36cd49a129ec9b016a80c4eb2487490130f76c
colin <382790119@qq.com>
2015-12-16 17:55:01 +0800

    init ccplay sdk

---
+++ sdks/ccplay/src/org/weilan/LoginOutBroadcastReceiver.java
@@ -0,0 +1,23 @@
+package org.weilan;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * 登出广播
+ * 
+ * @author 巍
+ * 
+ */
+public class LoginOutBroadcastReceiver extends BroadcastReceiver {
+
+    private final static int CB_LOGOUT_SUCCESS  = 2;
+	public static final String LOGINOUT_ACTION = "CCPAY_LOGINOUT_ACTION";
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+        sdk.platformCallback(CB_LOGOUT_SUCCESS, "");
+	}
+}
