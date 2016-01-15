00fb9b4051f8af6dc5089315631a7e69922187eb
xiaou <xiaotjyu@gmail.com>
2016-01-15 16:47:23 +0800

    update oppo sdk

--- sdks/oppo/src/org/weilan/GameProxyImpl.java
+++ sdks/oppo/src/org/weilan/GameProxyImpl.java
@@ -16,181 +16,146 @@ import android.os.Message;
 import android.widget.Toast;
 import android.text.TextUtils;
 
-
-import com.nearme.gamecenter.open.api.ApiCallback;
-import com.nearme.gamecenter.open.api.GameCenterSDK;
-import com.nearme.gamecenter.open.api.GameCenterSettings;
-import com.nearme.gamecenter.open.api.PayInfo;
-import com.nearme.gamecenter.open.core.util.ImageLoader;
-import com.nearme.gamecenter.open.core.util.Util;
-import com.nearme.oauth.log.LogUtil;
-import com.nearme.oauth.model.NDouProductInfo;
-import com.nearme.oauth.model.UserInfo;
+import com.nearme.game.sdk.*;
+import com.nearme.game.sdk.callback.ApiCallback;
+import com.nearme.game.sdk.callback.GameExitCallback;
+import com.nearme.game.sdk.common.model.biz.PayInfo;
+import com.nearme.game.sdk.common.model.biz.ReportUserGameInfoParam;
+import com.nearme.game.sdk.common.model.biz.ReqUserInfoParam;
+import com.nearme.game.sdk.GameCenterSDK;
+import com.nearme.game.sdk.common.model.biz.GameCenterSettings;
+import com.nearme.game.sdk.common.util.AppUtil;
+import com.nearme.platform.opensdk.pay.PayResponse;
 
 public class GameProxyImpl extends GameProxy {
-    private Activity currentActivity;
-    private Object switchCustomParams;
-    private PayCallBack payCallBack;
+
+    public boolean supportCommunity() {
+        // 是否支持打开社区
+        return false;
+    }
 
     @Override
     public void applicationInit(Activity activity) {
-        Log.v("sdk", "applicationInit");
-        GameCenterSettings gameCenterSettings = new GameCenterSettings(
-                "${APPKEY}", "${APPSECRET}") {
-
-            @Override
-            public void onForceReLogin() {
-                // sdk由于某些原因登出,此方法通知cp,cp需要在此处清理当前的登录状态并重新请求登录.
-                // 可以发广播通知页面重新登录
-                userListerner.onLogout(null);
-            }
-            
-            @Override 
-            public void onForceUpgradeCancel() {
-                // 游戏自升级，后台有设置为强制升级，用户点击取消时的回调函数。
-                // 若开启强制升级模式 ，  一般要求不更新则强制退出游戏并杀掉进程。
-                // System.exit(0) or kill this process
-            }
-        };
-        // TODO for test old
-//        AccountAgent.useNewApi = true;
-        GameCenterSettings.isDebugModel = false;// 测试log开关
-        GameCenterSettings.isOritationPort = true;// 控制SDK activity的横竖屏 true为竖屏
-        GameCenterSettings.proInnerSwitcher = false;//是否支持游戏内切换账号
-        GameCenterSDK.init(gameCenterSettings, activity);
+		// 因为sdk插件中service activity跑在独立进程，这里只需要在主进程做一次初始化操作。
+		if("${PACKAGE_NAME}".equals(AppUtil.getCurrentProcessName(activity))){
+			String appKey = "${APPID}";
+			String appSecret = "${APPKEY}";
+			GameCenterSettings gameCenterSettings = new GameCenterSettings(
+					false,    // 网游固定为false
+					appKey,
+					appSecret, 
+					true,     // 调试开关 true 打印log，false 关闭log，正式上线请设置为false
+					false);   // 将游戏横竖屏状态传递给sdk， true为竖屏  false为横屏
+			GameCenterSDK.init(gameCenterSettings, activity);
+		}
     }
 
     @Override
     public void onResume(Activity activity) {
         super.onResume(activity);
-        GameCenterSDK.setmCurrentContext(activity);
-        GameCenterSDK.getInstance().doShowSprite(activity);//显示浮标
+        GameCenterSDK.getInstance().onResume(activity);
     }
 
     @Override
     public void onPause(Activity activity) {
+        GameCenterSDK.getInstance().onPause();
         super.onPause(activity);
-        GameCenterSDK.getInstance().doDismissSprite(activity);
-    }
-
-    @Override
-    public void onCreate(Activity activity) {
-        Log.v("sdk", "onCreate");
     }
 
     @Override
     public void login(final Activity activity, final Object customParams) {
         // 调用SDK执行登陆操作
-        Log.v("sdk", "login");
-        currentActivity = activity;
-        GameCenterSDK.setmCurrentContext(activity);
-
-        GameCenterSDK.getInstance().doLogin(new ApiCallback() {
-
+        GameCenterSDK.getInstance().doLogin(activity, new ApiCallback() {
             @Override
-            public void onSuccess(String content, int code) {
-                try{
-                    JSONObject tokenObj = new JSONObject(GameCenterSDK.getInstance().doGetAccessToken());
-                    String oauth_token        = tokenObj.optString("oauth_token");
-                    String oauth_token_secret = tokenObj.optString("oauth_token_secret");
-                    String token = "oauth_token=" + oauth_token + "&oauth_token_secret=" + oauth_token_secret;
-                    User u = new User();
-                    u.token = token;
-                    userListerner.onLoginSuccess(u, customParams);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+            public void onSuccess(String content) {
+                GameCenterSDK.getInstance().doGetTokenAndSsoid(activity, new ApiCallback() {
+                    @Override
+                    public void onSuccess(String resultMsg) {
+                        try {
+                            JSONObject json = new JSONObject(resultMsg);
+                            String token = json.getString("token");
+                            String ssoid = json.getString("ssoid");
+
+                            User usr = new User();
+                            usr.token = token;
+                            usr.userID = ssoid;
+                            userListerner.onLoginSuccess(usr, customParams);
+                        } catch (JSONException e) {
+                            userListerner.onLoginFailed("parser token json failed", customParams);
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(String resultMsg, int resultCode) {
+                        userListerner.onLoginFailed("get token failed", customParams);
+                    }
+                });
             }
 
             @Override
             public void onFailure(String content, int code) {
-                //Toast.makeText(currentActivity, content, Toast.LENGTH_LONG).show();
                 userListerner.onLoginFailed(content, customParams);
             }
-        }, activity);
+        });
     }
 
     @Override
-    public void logout(Activity activity, Object customParams) {
-        Log.v("sdk", "logout");
-        userListerner.onLogout(customParams);
-    }
-
-    @Override
-    public void switchAccount(Activity activity, Object customParams) {
-        switchCustomParams = customParams;
-    }
-
-    private ApiCallback kebiPayment = new ApiCallback() {
-        @Override
-        public void onSuccess(String content, int code) {
-            payCallBack.onSuccess("支付成功");
-        }
-
-        @Override
-        public void onFailure(String content, int code) {
-            payCallBack.onFail(content + "," + code);
-            //Toast.makeText(currentActivity, "消耗可币失败:" + content, Toast.LENGTH_LONG).show();
-        }
-    };
-
-    @Override
-    public void pay(Activity activity, String ID, String name, String orderID, float price, String callBackInfo, JSONObject roleInfo, PayCallBack payCallBack) {
-        Log.v("sdk", "pay:" + ID + "," + name + "," + price + "," + callBackInfo + "," + roleInfo.toString());
-        currentActivity = activity;
-        this.payCallBack = payCallBack;
-
+    public void pay(Activity activity, String ID, String name, String orderID, float price, String callBackInfo, JSONObject roleInfo,final PayCallBack payCallBack) {
         final PayInfo payInfo = new PayInfo(orderID, callBackInfo, (int)price * 100);
         payInfo.setProductDesc(name);
         payInfo.setProductName(name);
         payInfo.setCallbackUrl("${PAY_URL}");
-        GameCenterSDK.getInstance().doKebiPay(kebiPayment, payInfo, activity);
+        GameCenterSDK.getInstance().doPay(activity, payInfo, new ApiCallback() {
+            @Override
+            public void onSuccess(String resultMsg) {
+                // 支付成功
+                payCallBack.onSuccess(resultMsg);
+            }
+
+            @Override
+            public void onFailure(String resultMsg, int resultCode) {
+                // 支付失败
+                payCallBack.onFail(resultMsg);
+            }
+        });
     }
 
     @Override
-    public void exit(Activity activity, ExitCallback callback) {
-        Log.v("sdk", "exit");
-        callback.onNo3rdExiterProvide();
+    public void exit(Activity activity,final ExitCallback callback) {
+        GameCenterSDK.getInstance().onExit(activity, new GameExitCallback(){
+            @Override
+            public void exitGame() {
+                // CP 实现游戏退出操作，也可以直接调用 AppUtil工具类里面的实现直接强杀进程~
+                //AppUtil.exitGameProcess(DemoActivity.this);  
+                callback.onNo3rdExiterProvide();
+            }
+        });
     }
-
-    private ApiCallback mExtInfoCallBack = new ApiCallback() {
-            @Override
-            public void onSuccess(String content, int code) {
-                // TODO Auto-generated method stub
-                //Util.makeToast("提交角色信息成功：" + content + "#" + code,
-                //        OpenSDKDemoNewActivity.this);
-            }
-        
-            @Override
-            public void onFailure(String content, int code) {
-                // TODO Auto-generated method stub
-                //Util.makeToast("提交角色信息失败：" + content, OpenSDKDemoNewActivity.this);
-            }
-    };
 
     @Override
     public void setExtData(Context context, String ext) {
-        Log.v("sdk", "setExtData:" + ext);
         try {
             JSONObject src = new JSONObject(ext);
-            String extendInfo = new StringBuilder()
-                .append("gameId=").append(((poem)currentActivity).getMetaData("appid"))
-                .append("&service=").append(src.getString("serverID"))
-                .append("&role=").append(src.getString("name"))
-                .append("&grade=").append(src.getString("level")).toString();
 
-            GameCenterSDK.getInstance().doSubmitExtendInfo(mExtInfoCallBack,
-                    extendInfo, currentActivity);
+            GameCenterSDK.getInstance().doReportUserGameInfoData(context,
+                    new ReportUserGameInfoParam("${APPID}"
+                        , src.getString("serverID")
+                        , src.getString("name")
+                        , src.getString("level")),new ApiCallback() {
+
+                        @Override
+                        public void onSuccess(String resultMsg) {
+                        }
+
+                        @Override
+                        public void onFailure(String resultMsg, int resultCode) {
+                        }
+            });
         } catch (Exception e) {
             // 处理异常
             Log.e("sdk", "set Ext Data exception");
             e.printStackTrace();
         }
-    }
-
-    @Override
-    public void openCommunity(Activity activity) {
-        GameCenterSDK.setmCurrentContext(activity);
-        GameCenterSDK.getInstance().doShowGameCenter(activity);
     }
 }
